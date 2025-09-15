02cc01eba117c0b293672e84260dc2f5d11372ea
Isabela Duetes <isabela.duetes@aluno.senai.br>
2025-09-15 10:53:34 -0300

    feat: finaliza select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,6 +10,9 @@ CREATE TABLE artistas (
     ativo BOOLEAN DEFAULT TRUE
 );
 
-INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativo),
+INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativo)
 VALUES
 ('One Direction','Banda', 5, 'Pop', 'Reino Unido', 2010, 'You & I', FALSE),
+('Queen','Banda', 4, 'Rock', 'Reino Unido', 1970, 'Bohemian Rhapsody', FALSE),
+('Beyoncé','Artista Solo', 1, 'R&B', 'Estados Unidos', 1997, 'Single Ladies (Put a Ring on It)', TRUE),
+('BTS','Banda', 7, 'K-Pop', 'Coreia do Sul', 2013, 'Dynamite', TRUE);
